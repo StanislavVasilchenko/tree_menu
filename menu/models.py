4109f172b70f684c8c136f7600db9f338d8e8e93
stanislav <stanislav.vasilchenko@yandex.ru>
2024-03-24 13:14:43 +0300

    create model menu

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    title = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    url = models.URLField()
+
+    def __str__(self):
+        return f'{self.title} - {self.parent}'
+
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'MENU'
